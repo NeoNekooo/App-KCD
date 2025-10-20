9b4a5d310c07e79b547a67de7f61bbde944b92a9
Markmhbr <markmhbr@example.com>
2025-10-20 16:18:16 +0700

    Perbaikan Terbaru

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,7 +38,7 @@ class AppServiceProvider extends ServiceProvider
 
         // Mengirim data KontakPpdb ke semua view
         if (Schema::hasTable('kontak_ppdbs')) {
-            $kontak = KontakPpdb::first();
+            $kontakPpdb = KontakPpdb::first();
             view()->share('kontakPpdb', $kontakPpdb);
         }
 
