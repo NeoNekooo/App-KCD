49aaa6a53e9e86f0833d49f3dfe9aaa511a21a4e
Markmhbr <markmhbr@example.com>
2025-12-09 09:22:07 +0700

    Perbaikan

--- resources/views/admin/underConstruction.blade.php
+++ resources/views/admin/underConstruction.blade.php
@@ -2,9 +2,16 @@
 
 @section('content')
 
-    <div class="alert alert-danger alert-dismissible" role="alert">
-      <strong>Halaman Sedang dalam Perbaikan</strong>.
-      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <!--Under Maintenance -->
+<div class="container-xxl container-p-y text-center">
+    <div class="misc-wrapper">
+        <h3 class="mb-2 mx-2">Sedang Proses Pengembangan!</h3>
+        <p class="mb-6 mx-2">Mohon maaf atas ketidaknyamanannya, namun saat ini kami sedang melakukan proses pengembangan</p>
+        
+        <div class="mt-6 text-center">
+            <img src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template-free/demo/assets/img/illustrations/girl-doing-yoga-light.png" alt="girl-doing-yoga-light" width="500" class="img-fluid text-center mx-auto d-block" />
+        </div>
     </div>
+</div>
     
 @endsection
