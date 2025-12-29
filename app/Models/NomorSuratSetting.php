8d39c63418c57327d0de827a8c51269b775b542b
Markmhbr <markmhbr@example.com>
2025-12-29 15:34:43 +0700

    Perbaikan

---
+++ app/Models/NomorSuratSetting.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class NomorSuratSetting extends Model {
+    protected $table = 'nomor_surat_settings';
+    protected $guarded = ['id'];
+}
