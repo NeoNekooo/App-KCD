83e31abbe7ff0ef4644d2a5c8cdec7386e8f5df3
NeoNekooo <lgpplg7@gmail.com>
2026-04-23 11:53:41 +0700

    wlee

--- app/Http/Controllers/Admin/Kepegawaian/PegawaiKcdController.php
+++ app/Http/Controllers/Admin/Kepegawaian/PegawaiKcdController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rule;
 use App\Models\Instansi;
 
 class PegawaiKcdController extends Controller
