e9e9f65ec0e986b95248bd799e04bf0c54d3795b
NeoNekooo <lgpplg7@gmail.com>
2026-04-23 09:15:47 +0700

    wlee

--- routes/web.php
+++ routes/web.php
@@ -461,8 +461,8 @@ Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () 
         Route::resource('sliders', SliderController::class)->middleware('check_menu:web-slider');
         Route::get('welcome', [WelcomeMessageController::class, 'index'])->name('welcome.index')->middleware('check_menu:web-welcome');
         Route::post('welcome', [WelcomeMessageController::class, 'update'])->name('welcome.update')->middleware('check_menu:web-welcome');
-        // Route::get('settings', [WebSettingController::class, 'index'])->name('settings.index')->middleware('check_menu:web-settings');
-        // Route::post('settings', [WebSettingController::class, 'update'])->name('settings.update')->middleware('check_menu:web-settings');
+        Route::get('settings', [WebSettingController::class, 'index'])->name('settings.index');
+        Route::post('settings', [WebSettingController::class, 'update'])->name('settings.update');
         });
     Route::get('/underConstructions', function () {
         return view('admin.underConstruction');
