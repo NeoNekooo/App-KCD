f9723459cc7435db710b0d4999f46ecfe49ef058
NeoNekooo <lgpplg7@gmail.com>
2026-04-23 12:56:15 +0700

    wlee

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -60,5 +60,16 @@ class User extends Authenticatable
     {
         // Pastikan model \App\Models\PegawaiKcd sudah ada
         return $this->belongsTo(PegawaiKcd::class, 'pegawai_kcd_id');
+    }
+
+    /**
+     * 🔥 CUSTOM ROLE CHECKER (Fix Error hasRole)
+     */
+    public function hasRole($role)
+    {
+        if (strtolower($role) === 'super admin') {
+            return strtolower($this->role) === 'administrator';
+        }
+        return strtolower($this->role) === strtolower($role);
     }
 }
