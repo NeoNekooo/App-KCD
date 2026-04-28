98680d0b0311a4e6fddd16d243f2a6951c53ecb2
NeoNekooo <lgpplg7@gmail.com>
2026-04-28 14:20:57 +0700

    wlee

--- resources/views/auth/2fa_verify.blade.php
+++ resources/views/auth/2fa_verify.blade.php
@@ -126,16 +126,16 @@
                             </div>
                         @endif
 
-                        <form id="otpForm" action="{{ route('2fa.verify') }}" method="POST">
+                        <form id="otpForm" action="{{ route('2fa.verify') }}" method="POST" autocomplete="off">
                             @csrf
                             <input type="hidden" name="one_time_password" id="fullOtp">
                             <div class="digit-group d-flex justify-content-center mb-4" autocomplete="off">
-                                <input type="text" id="digit-1" name="digit-1" data-next="digit-2" maxlength="1" inputmode="numeric" pattern="[0-9]*" />
-                                <input type="text" id="digit-2" name="digit-2" data-next="digit-3" data-previous="digit-1" maxlength="1" inputmode="numeric" pattern="[0-9]*" />
-                                <input type="text" id="digit-3" name="digit-3" data-next="digit-4" data-previous="digit-2" maxlength="1" inputmode="numeric" pattern="[0-9]*" />
-                                <input type="text" id="digit-4" name="digit-4" data-next="digit-5" data-previous="digit-3" maxlength="1" inputmode="numeric" pattern="[0-9]*" />
-                                <input type="text" id="digit-5" name="digit-5" data-next="digit-6" data-previous="digit-4" maxlength="1" inputmode="numeric" pattern="[0-9]*" />
-                                <input type="text" id="digit-6" name="digit-6" data-previous="digit-5" maxlength="1" inputmode="numeric" pattern="[0-9]*" />
+                                <input type="text" id="digit-1" name="digit-1" data-next="digit-2" maxlength="1" inputmode="numeric" pattern="[0-9]*" autocomplete="one-time-code" />
+                                <input type="text" id="digit-2" name="digit-2" data-next="digit-3" data-previous="digit-1" maxlength="1" inputmode="numeric" pattern="[0-9]*" autocomplete="one-time-code" />
+                                <input type="text" id="digit-3" name="digit-3" data-next="digit-4" data-previous="digit-2" maxlength="1" inputmode="numeric" pattern="[0-9]*" autocomplete="one-time-code" />
+                                <input type="text" id="digit-4" name="digit-4" data-next="digit-5" data-previous="digit-3" maxlength="1" inputmode="numeric" pattern="[0-9]*" autocomplete="one-time-code" />
+                                <input type="text" id="digit-5" name="digit-5" data-next="digit-6" data-previous="digit-4" maxlength="1" inputmode="numeric" pattern="[0-9]*" autocomplete="one-time-code" />
+                                <input type="text" id="digit-6" name="digit-6" data-previous="digit-5" maxlength="1" inputmode="numeric" pattern="[0-9]*" autocomplete="one-time-code" />
                             </div>
                             <button type="submit" class="btn btn-primary d-grid w-100 btn-verifikasi">Verifikasi Sekarang</button>
                             <p class="text-center mt-3">
