4a7a68d0fe3e50d5ba0eb585de8657d6d4d4debb
NeoNekooo <lgpplg7@gmail.com>
2026-05-03 18:33:37 +0700

    wlee

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ $appSettings['site_name'] ?? 'MANDALA' }} | @yield('title')</title>
+    <title>@yield('title') | {{ $appSettings['site_name'] ?? 'MANDALA' }}</title>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.ico') }}" />
 
     <link rel="stylesheet" href="{{ asset('vendor/fonts/boxicons.css') }}" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
